5b6f7cfa620f260806eadbda2cc5a783519a616c
Pedro Pareschi <pedropareschi15@gmail.com>
2021-06-03 19:34:22 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/pedropareschi/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package br.com.pedropareschi.workshopmongo.services;
+
+import br.com.pedropareschi.workshopmongo.domain.Post;
+import br.com.pedropareschi.workshopmongo.repositories.PostRepository;
+import br.com.pedropareschi.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public List<Post> findAll(){
+        return  repo.findAll();
+    }
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    
+}
